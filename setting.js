49fa0ca787a3a568974956404251cbdd82bd5564
azeditx01-cloud <azeditx01@gmail.com>
2026-04-15 12:06:09 +0500

    Update setting.js

--- setting.js
+++ setting.js
@@ -3,10 +3,10 @@
 // habari yako 😂 😂
 
 module.exports = {
-  BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ', 
-  OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
-  SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
+  BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-AHMAD', 
+  OWNER_NAME: process.env.OWNER_NAME || 'Mr Ahmad', 
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '923411554654', 
+  SESSION_ID: process.env.SESSION_ID || 'blinder~S2QGDbZJ#a-qd_bD1Df_qlFdBL5Cw70ER5m-D5HkS5qFq_lbz02q4', // paste here your blinders session ID
   MODE: process.env.MODE || "private", // public or private
-  TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
+  TIMEZONE: process.env.TIMEZONE || 'Asia/Karachi' 
 };
